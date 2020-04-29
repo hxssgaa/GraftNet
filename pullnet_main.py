735d7fe5a2db1a152261b5700e9f262a5210e461
Huang Xin <huangx2@i2r.a-star.edu.sg>
2020-04-29 13:34:37 +0800

    webqsp

--- pullnet_main.py
+++ pullnet_main.py
@@ -324,7 +324,7 @@ def train_relreasoner(cfg, is_entity=False):
         train_data = RelReasonerDataLoader(cfg['data_folder'] + cfg['train_data'], facts, features, num_hop,
                                            word2id, relation2id, cfg['max_query_word'], cfg['use_inverse_relation'], 1, teacher_force=True)
 
-        valid_data = RelReasonerDataLoader(cfg['data_folder'] + cfg['test_data'], facts, features, num_hop,
+        valid_data = RelReasonerDataLoader(cfg['data_folder'] + cfg['dev_data'], facts, features, num_hop,
                                            word2id, relation2id, cfg['max_query_word'], cfg['use_inverse_relation'], 1, teacher_force=False)
     else:
         train_data = RelReasonerObjectDataLoader(cfg['data_folder'] + cfg['train_data'], facts, features, num_hop,
